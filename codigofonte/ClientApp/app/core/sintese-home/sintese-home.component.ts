b0cc0e1587d171b87a7d70a991061986793d1413
Rafael Rocha <rafael.rocha@ibge.gov.br>
2017-11-24 13:41:40 -0200

    Componente sintese-home

---
+++ codigofonte/ClientApp/app/core/sintese-home/sintese-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sintese-home',
+    templateUrl: './sintese-home.component.html',
+    styleUrls: ['./sintese-home.component.css']
+})
+export class SinteseHomeComponent {
+    public teste = 'hello world';
+}
