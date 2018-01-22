92cbaa48c3a511a9f5687bf4b9efdb06fc584295
Elias Correa Silva <elias.silva@ibge.gov.br>
2018-01-22 18:41:02 -0200

    Criação do componente de dodos do país.

---
+++ codigofonte/ClientApp/app/dados-pais/dados-pais.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'dados-pais',
+    templateUrl: './dados-pais.component.html',
+    styleUrls: ['./dados-pais.component.css']
+})
+export class DadosPaisComponent {
+}
