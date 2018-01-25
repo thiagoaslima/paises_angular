ccc82024ae383ee9b9a4bd733d18aed9cec241b8
Elias Correa Silva <elias.silva@ibge.gov.br>
2018-01-25 17:17:55 -0200

    Implementação da sintese do país no componente dos dados-país.

--- codigofonte/ClientApp/app/dados-pais/dados-pais.component.ts
+++ codigofonte/ClientApp/app/dados-pais/dados-pais.component.ts
@@ -1,9 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { ParamMap, ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs/Subscription';
+
+import { RouterParamsService, LocalidadeService, Pais } from "../shared";
+import { PaisesService } from "../shared/paises.service";
+import { SinteseHomeService } from "../mapa-section/sintese-home/sintese-home.service";
+import { ItemTemaComponent } from "../sandbox/componentes/item-tema.component";
 
 @Component({
     selector: 'dados-pais',
     templateUrl: './dados-pais.component.html',
-    styleUrls: ['./dados-pais.component.css']
+    styleUrls: ['./dados-pais.component.css'],
+    providers: [SinteseHomeService],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DadosPaisComponent {
+    public pais: Pais | null = null;
+    public imageSrc = ''
+    public itens = <any[]>[];
+
+    historico_aberto = false;
+
+    private _subscriptions: {
+        [key: string]: Subscription
+    } =  Object.create(null);
+
+    constructor(
+        private _routerParams: RouterParamsService,
+        private _localidadeService: LocalidadeService,
+        private _sinteseService: SinteseHomeService,
+		private _changeDetector: ChangeDetectorRef
+    ) { }
+
+    ngOnInit() {
+        this._subscriptions.params = this._routerParams.params$.subscribe(({ params, url }) => {
+            let pais = this._localidadeService.getPaisBySlug(params.pais);
+            this.itens.length = 0;
+            this.setImageSrc(pais);
+
+            if (pais) {
+                this.pais = pais;
+
+                this._sinteseService.getSintese(pais.sigla).subscribe((resultados: any) => {
+                    this.itens.push(...resultados);
+					this._changeDetector.detectChanges();
+                });
+            } else {
+                this.pais = null;
+            }
+            console.log(pais);
+        });
+    }
+
+    ngOnDestroy() {
+        this._changeDetector.detach();
+        Object.values(this._subscriptions).forEach(subscription => subscription.unsubscribe());
+    }
+
+    setImageSrc(pais: Pais | null) {
+        if (pais) {
+            this.imageSrc = 'img/bandeiras/' + pais.sigla.toUpperCase() + '.gif';
+        } else {
+            this.imageSrc = '';
+        }
+    }
 }
