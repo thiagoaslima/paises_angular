47d294efdad8aea6b9d628e0ae16fc7c41385ca1
Thiago Lima <thiagoaslima@gmail.com>
2018-03-08 19:11:32 -0300

    lista paises verificada

--- meta-arquivos/lista-paises/scripts/diff.js
+++ meta-arquivos/lista-paises/scripts/diff.js
@@ -1,14 +1,56 @@
 const fs = require('fs');
+const IBGE = require('../fontes/IBGE_2015.json').dados;
 const ISO = require('../fontes/ISO.json').dados;
 const ONU_EN = require('../fontes/UNSD_en.json').dados;
 
-const iso = ISO.map(obj => obj.code_a3).sort();
-const onu = ONU_EN.map(obj => obj["ISO-alpha3 Code"]).sort();
+const _ibge = IBGE.map(obj => obj.a3).sort();
+const _iso = ISO.map(obj => obj.code_a3).sort();
+const _onu = ONU_EN.map(obj => obj["ISO-alpha3 Code"]).sort();
 
-const iso_only = iso.filter(sigla => !onu.includes(sigla))
-const onu_only = onu.filter(sigla => !iso.includes(sigla))
+const todos = new Set([].concat(_ibge, _iso, _onu));
 
-fs.writeFile('diff.json', JSON.stringify({iso, onu, only: {iso: iso_only, onu: onu_only}}), 'utf8', (err) => {
+const ibge = (function () {
+    let comp = new Set([].concat(_iso, _onu));
+
+    return {
+        todos: _ibge,
+        faltantes: Array.from(todos.values()).filter(sigla => !_ibge.includes(sigla)),
+        exclusivos: _ibge.filter(sigla => !comp.has(sigla))
+    }
+})()
+
+const iso = (function () {
+    let comp = new Set([].concat(_ibge, _onu));
+
+    return {
+        todos: _iso,
+        faltantes: Array.from(todos.values()).filter(sigla => !_iso.includes(sigla)),
+        exclusivos: _iso.filter(sigla => !comp.has(sigla))
+    }
+})()
+
+const onu = (function () {
+    let comp = new Set([].concat(_iso, _ibge));
+
+    return {
+        todos: _onu,
+        faltantes: Array.from(todos.values()).filter(sigla => !_onu.includes(sigla)),
+        exclusivos: _onu.filter(sigla => !comp.has(sigla))
+    }
+})()
+
+const diff_content = {
+    ibge,
+    iso,
+    onu
+};
+
+fs.writeFile('diff.json', JSON.stringify(diff_content, null, 4), 'utf8', (err) => {
     if (err) console.log(err);
-    console.log('sucess');
+    console.log('diff sucess');
+})
+
+fs.writeFile('lista-siglas-a3.json', JSON.stringify(Array.from(todos.values())), 'utf8', (err) => {
+    if (err) console.log(err);
+    console.log('lista sucess');
 })
--- meta-arquivos/lista-paises/scripts/merge.js
+++ meta-arquivos/lista-paises/scripts/merge.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
-const slug = require('slugify');
+const slugify = require('slugify');
+
+function slug(str) {
+    str = str.replace(/['`"´’]/g, "")
+    return slugify(str, {
+        replacement: '-',
+        remove: null,
+        lower: true
+    });
+}
 
 const readFile = util.promisify(fs.readFile);
 
 const ISO = require('../fontes/ISO.json').dados;
 const ONU_EN = require('../fontes/UNSD_en.json').dados;
 const ONU_ES = require('../fontes/UNSD_es.json').dados;
-const PAISES = require('../fontes/paises.json');
-const nomesONU = [
-  "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia (Plurinational State of)", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Côte D'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic People's Republic of Korea", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia (Republic of The)", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Lao People’s Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia (Federated States of)", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Republic of Korea", "Republic of Moldova", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Tajikistan", "Thailand", "The former Yugoslav Republic of Macedonia", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom of Great Britain and Northern Ireland", "United Republic of Tanzania", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela, Bolivarian Republic of", "Viet Nam", "Yemen", "Zambia", "Zimbabwe", "Holy See", "State of Palestine"
-];
-const slugONU = nomesONU.map(slug);
+const IBGE = require('../fontes/IBGE_2015.json').dados;
+const ONU_MEMBROS = require('../fontes/ONU_membros.json').dados;
+const WIKIPEDIA = require('../fontes/Wikipedia.json');
 
 const mapONU_EN = ONU_EN.reduce((agg, obj) => {
     agg[obj["ISO-alpha3 Code"]] = obj;
@@ -22,113 +29,147 @@ const mapONU_ES = ONU_ES.reduce((agg, obj) => {
     agg[obj["ISO-alpha3 Code"]] = obj;
     return agg;
 }, {});
+const mapIBGE = IBGE.reduce((agg, obj, idx) => {
+    let sigla = obj.a3 || '_' + idx;
+    agg[sigla] = obj;
+    return agg;
+}, {});
+const mapISO = ISO.reduce((agg, obj) => {
+    let sigla = obj["code_a3"];
+    agg[sigla] = obj;
+    return agg;
+}, {});
+const slugMembrosONU = [].concat(ONU_MEMBROS.membros, ONU_MEMBROS.observadores.permanentes).map(slug);
 
-let regioes = ONU_EN.reduce((agg, obj) => {
-    const slug_en = slug(obj["Country or Area"]);
-    const es = mapONU_ES[obj["ISO-alpha3 Code"]];
-    const slug_es = slug(es["Country or Area"]);
-    
-    if (obj["Intermediate Region Name"] && !agg[obj["Intermediate Region Name"]]) {
-        const pt = PAISES.regioes.find(regiao => regiao.nome.en === obj["Intermediate Region Name"]);
+let regioes = {
+    "World": {
+        "tipo": "mundo",
+        "nome": {
+            "en": "World",
+            "es": "Mundo",
+            "pt": "Mundo"
+        },
+        "slug": {
+            "en": "world",
+            "es": "mundo",
+            "pt": "mundo"
+        },
+        "codigo": 1,
+        "parent": null
+    }
+};
 
-        agg[obj["Intermediate Region Name"]] = {
+ONU_EN.forEach((obj) => {
+    let sigla = obj["ISO-alpha3 Code"];
+
+    if (
+        obj["Intermediate Region Name"] && 
+        !regioes[obj["Intermediate Region Name"]]
+    ) {
+        const es = mapONU_ES[sigla];
+
+        const nome_pt = WIKIPEDIA.regioes_pt[obj["Intermediate Region Code"]];
+        const slug_pt = slug(nome_pt);
+
+        regioes[obj["Intermediate Region Name"]] = {
             "tipo": "subregiao",
+            "slug": slug_pt,
             "nome": {
                 "en": obj["Intermediate Region Name"],
                 "es": es["Intermediate Region Name"],
-                "pt": pt ? pt.nome.en : ""
+                "pt": nome_pt
             },
-            "slug": {
-                "en": slug_en,
-                "es": slug_es,
-                "pt": slug(pt ? pt.nome.en : "") 
+            "slugs": {
+                "en": slug(obj["Intermediate Region Name"]),
+                "es": slug(es["Intermediate Region Name"]),
+                "pt": slug_pt
             },
             "codigo": obj["Intermediate Region Code"],
             "parent": obj["Sub-region Code"]
         }
     }
 
-    if (obj["Sub-region Name"] && !agg[obj["Sub-region Name"]]) {
-        const pt = PAISES.regioes.find(regiao => regiao.nome.en === obj["Sub-region Name"]);
+    if (
+        obj["Sub-region Name"] && 
+        !regioes[obj["Sub-region Name"]]
+    ) {
+        const es = mapONU_ES[sigla];
+        const nome_pt = WIKIPEDIA.regioes_pt[obj["Sub-region Code"]];
+        const slug_pt = slug(nome_pt);
 
-        agg[obj["Sub-region Name"]] = {
+        regioes[obj["Sub-region Name"]] = {
             "tipo": "subregiao",
+            "slug": slug_pt,
             "nome": {
                 "en": obj["Sub-region Name"],
                 "es": es["Sub-region Name"],
-                "pt": pt ? pt.nome.en : ""
+                "pt": nome_pt
             },
-            "slug": {
-                "en": slug_en,
-                "es": slug_es,
-                "pt": slug(pt ? pt.nome.en : "") 
+            "slugs": {
+                "en": slug(obj["Sub-region Name"]),
+                "es": slug(es["Sub-region Name"]),
+                "pt": slug_pt
             },
             "codigo": obj["Sub-region Code"],
             "parent": obj["Region Code"]
         }
     }
 
-    if (obj["Region Name"] && !agg[obj["Region Name"]]) {
-        const pt = PAISES.regioes.find(regiao => regiao.nome.en === obj["Region Name"]);
+    if (
+        obj["Region Name"] && 
+        !regioes[obj["Region Name"]]
+    ) {
+        const es = mapONU_ES[sigla];
+        const nome_pt = WIKIPEDIA.regioes_pt[obj["Region Code"]];
+        const slug_pt = slug(nome_pt);
 
-        agg[obj["Region Name"]] = {
+        regioes[obj["Region Name"]] = {
             "tipo": "subregiao",
+            "slug": slug_pt,
             "nome": {
                 "en": obj["Region Name"],
                 "es": es["Region Name"],
-                "pt": pt ? pt.nome.en : ""
+                "pt": nome_pt
             },
             "slug": {
-                "en": slug_en,
-                "es": slug_es,
-                "pt": slug(pt ? pt.nome.en : "") 
+                "en": slug(obj["Region Name"]),
+                "es": slug(es["Region Name"]),
+                "pt": slug_pt
             },
             "codigo": obj["Region Code"],
             "parent": 1
         }
     }
 
-    return agg;
-}, {
-        "World": {
-            "tipo": "mundo",
-            "nome": {
-                "en": "World",
-                "es": "Mundo",
-                "pt": "Mundo"
-            },
-            "slug": {
-                "en": "world",
-                "es": "mundo",
-                "pt": "mundo"
-            },
-            "codigo": 1,
-            "parent": null
-        }
-    }
-);
+});
+
 
 let paises = ISO.map(obj => {
-    console.log(obj, obj.code_a3);
-    const en = mapONU_EN[obj['code_a3']];
-    const slug_en = en ? slug(en["Country or Area"]) : "";
-    const es = mapONU_ES[obj.code_a3];
-    const slug_es = es ? slug(es["Country or Area"]) : "";
+    const sigla = obj["code_a3"];
 
-    const pt = PAISES.paises.find(pais => pais.sigla3 === obj.code_a3);
+    const en = mapONU_EN[sigla];
+    const es = mapONU_ES[sigla];
+    const pt = mapIBGE[sigla];
+
+    const nome_en = en ? en["Country or Area"] : obj["name_en"] ? obj["name_en"] : "";
+    const nome_es = es ? es["Country or Area"] : WIKIPEDIA.paises_es[sigla] ? WIKIPEDIA.paises_es[sigla] : "";
+    const nome_pt = pt ? pt.nome : WIKIPEDIA.paises_pt[sigla] ? WIKIPEDIA.paises_pt[sigla] : "";
+    const slug_pt = slug(nome_pt);
+
     return {
         "codigo": obj.numeric,
-        "sigla": obj.sigla,
-        "sigla3": obj.sigla3,
+        "slug": slug_pt,
+        "sigla": obj["code_a2"],
+        "sigla3": obj["code_a3"],
         "nome": {
-            "en": en ? en["Country or Area"] : obj["nome_en"],
-            "es": es ? es["Country or Area"] : "",
-            "pt": pt ? pt.nome.pt : ""
+            "en": nome_en,
+            "es": nome_es,
+            "pt": nome_pt
         },
-        "slug": {
-            "en": slug_en,
-            "es": slug_es,
-            "pt": slug(pt ? pt.nome.pt : "")
+        "slugs": {
+            "en": slug(nome_en),
+            "es": slug(nome_es),
+            "pt": slug_pt
         },
         "parent": en ? en["Intermediate Region Code"] || en["Sub-region Code"] || en["Region Code"] || en["Global Code"] : "",
         "parents": {
@@ -136,8 +177,7 @@ let paises = ISO.map(obj => {
             "regiao": en ? en["Sub-region Code"] : "",
             "subregiao": en ? en["Intermediate Region Code"] : ""
         },
-        "ddi": pt ? pt.ddi : "",
-        "onu": slugONU.includes(slug_en)
+        "onu": slugMembrosONU.indexOf(slug(nome_en)) > -1
     }
 });
 
@@ -253,13 +293,76 @@ const metadados = {
                 "link": "http://www.un.org/en/member-states/index.html",
                 "acessadoEm": "2018-03-07"
             }
-        }
+        },
+        "notas": [
+            {
+                "nota": "Nome da Antártida (ATA) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ant%C3%A1rtida",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome da Ilha Bouvet (BVT) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilha_Bouvet",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome da Ilha Christmas (CXR) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilha_Christmas",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome das Ilha Heard e Ilhas McDonald (Keeling) (HMD) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilha_Heard_e_Ilhas_McDonald",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome das Ilhas Cocos (Keeling) (CCK) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilhas_Cocos_(Keeling)",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome das Ilhas Geórgia do Sul e Sandwich do Sul (SGS) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilhas_Geórgia_do_Sul_e_Sandwich_do_Sul",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome das Ilhas Menores Distantes dos Estados Unidos (UMI) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Ilhas_Menores_Distantes_dos_Estados_Unidos",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome de Taiwan (TWN) em português e espanhol foi pego da Wikipedia",
+                "fontes": [
+                    "https://pt.wikipedia.org/wiki/Taiwan",
+                    "https://es.wikipedia.org/wiki/República_de_China"
+                ],
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome das Terras Austrais e Antárticas Francesas (ATF) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Terras_Austrais_e_Ant%C3%A1rticas_Francesas",
+                "acessadoEm": "2018-03-08"
+            },
+            {
+                "nota": "Nome do Território Britânico do Oceano Índico (IOT) em português foi pego da Wikipedia",
+                "fonte": "https://pt.wikipedia.org/wiki/Territ%C3%B3rio_Brit%C3%A2nico_do_Oceano_%C3%8Dndico",
+                "acessadoEm": "2018-03-08"
+            }
+        ]
     }
 }
 
-fs.writeFile(path.resolve('..', 'compilado', 'paises.json'), JSON.stringify(paises, null, 4), (err) => {
+const paisesFile = paises.map(pais => {
+    let _pais = Object.assign({}, pais);
+    delete _pais.parent,
+    delete _pais.parents
+    return _pais;
+})
+
+fs.writeFile(path.resolve('..', 'compilado', 'paises.json'), JSON.stringify(paisesFile, null, 4), (err) => {
     console.log('paises pronto');
 });
-fs.writeFile(path.resolve('..', 'compilado', 'dados_completos.json'), JSON.stringify({regioes, paises, metadados}, null, 4), (err) => {
+
+fs.writeFile(path.resolve('..', 'compilado', 'dados_completos.json'), JSON.stringify({paises, regioes}, null, 4), (err) => {
     console.log('dados_completos pronto');
 })
