1be7b620661dde95112cb31fce42708cb3432b74
Thiago Augusto Silva de Lima <thiago.lima@CAN00572820.local>
2019-01-04 18:32:13 -0200

    starting translate module

--- codigofonte/ClientApp/app/app.module.shared.ts
+++ codigofonte/ClientApp/app/app.module.shared.ts
@@ -1,26 +1,20 @@
-import { NgModule, Inject, PLATFORM_ID, APP_ID } from '@angular/core';
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
+import { NgModule, Inject, PLATFORM_ID, APP_ID } from "@angular/core";
+import { CommonModule, isPlatformBrowser } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from "./shared/shared.module";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app-component/app.component';
+import { AppComponent } from "./app-component/app.component";
 import { CoreModule } from "./core/core.module";
-import { cases, values } from './shared/specialCases.value';
+import { cases, values } from "./shared/specialCases.value";
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [
-        { provide: 'SPECIAL_VALUES', useValue: {cases, values} }
-    ],
+    declarations: [AppComponent],
+    providers: [{ provide: "SPECIAL_VALUES", useValue: { cases, values } }],
     imports: [
         CommonModule,
-        HttpModule,
         HttpClientModule,
         FormsModule,
         CoreModule,
@@ -31,9 +25,11 @@ import { cases, values } from './shared/specialCases.value';
 export class AppModuleShared {
     constructor(
         @Inject(PLATFORM_ID) private platformId: Object,
-        @Inject(APP_ID) private appId: string) {
-        const platform = isPlatformBrowser(platformId) ?
-            'on the server' : 'in the browser';
+        @Inject(APP_ID) private appId: string
+    ) {
+        const platform = isPlatformBrowser(platformId)
+            ? "on the server"
+            : "in the browser";
         console.log(`Running ${platform} with appId=${appId}`);
     }
 }
---
+++ codigofonte/ClientApp/app/shared/translate/translate.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, Inject } from "@angular/core";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+
+import { TranslateServiceConfig } from "./translate.module";
+import { TranslateConfig, AppSupportedLanguage } from "./translate.models";
+import { RouterParamsService } from "../router-params.service";
+import { map, filter, distinctUntilChanged } from "rxjs/operators";
+
+@Injectable()
+export class TranslateService {
+    current$: BehaviorSubject<string>;
+    languages$: BehaviorSubject<AppSupportedLanguage[]>;
+
+    constructor(
+        @Inject(TranslateServiceConfig) private config: TranslateConfig,
+        private routeParamsService: RouterParamsService
+    ) {
+        const initialLang = this.config.languages.reduce(
+            (agg, language) => (language.default ? language : agg)
+        );
+
+        this.current$ = new BehaviorSubject<string>(initialLang.id);
+        this.languages$ = new BehaviorSubject<AppSupportedLanguage[]>(
+            this.config.languages
+        );
+
+        this.routeParamsService.params$
+            .pipe(
+                map(params => params.queryParams.lang),
+                filter(Boolean),
+                distinctUntilChanged()
+            )
+            .subscribe(lang => this.current$.next(lang));
+    }
+}
