5426f7fdddb7623df6629f07b182e2ce9a83d156
Thiago Augusto Silva de Lima <thiago.lima@CAN00572820.local>
2019-03-21 17:47:09 -0300

    updates barra-gov component

--- codigofonte/ClientApp/app/core/barra-gov/barra-gov.component.ts
+++ codigofonte/ClientApp/app/core/barra-gov/barra-gov.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Inject,
+    ViewEncapsulation,
+    Input,
+    HostBinding,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
     selector: 'barra-gov',
     templateUrl: './barra-gov.component.html',
-    styleUrls: ['./barra-gov.component.css']
+    styleUrls: ['./barra-gov.component.css'],
+    encapsulation: ViewEncapsulation.Emulated,
 })
-export class BarraGovComponent {
-    
+export class BarraGovComponent implements OnInit {
+    CODIGO_IBGE = 3; // https://siorg.planejamento.gov.br/siorg-cidadao-webapp/pages/listar_orgaos_estruturas/listar_orgaos_estruturas.jsf#bxResultado
+
+    SCRIPTS: {
+        [idx: number]: { idg: number; url: string; loaded: boolean };
+    } = {
+        1: {
+            idg: 1,
+            url: '//barra.brasil.gov.br/barra.js',
+            loaded: false,
+        },
+        2: {
+            idg: 2,
+            url: '//barra.brasil.gov.br/barra_2.0.js',
+            loaded: false,
+        },
+    };
+
+    @Input('idg') set barra(value: number) {
+        this.idg = value;
+        this.loadScript(value);
+    }
+
+    @HostBinding('class.popup-layer') layer = true;
+
+    idg = 2;
+
+    constructor(@Inject(DOCUMENT) private document: Document) {}
+
+    ngOnInit() {
+        if (this.document && this.document.head) {
+            const meta = this.document.createElement('meta');
+            (<any>meta).property = 'creator.productor';
+            meta.content = `http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/${
+                this.CODIGO_IBGE
+            }`;
+            this.loadScript(this.idg);
+        }
+    }
+
+    private loadScript(idg: number): void {
+        if (!this.SCRIPTS[idg] || this.SCRIPTS[idg].loaded) {
+            return;
+        }
+
+        if (this.document && this.document.head) {
+            const script = this.document.createElement('script');
+            script.src = this.SCRIPTS[idg].url;
+            script.defer = true;
+            script.onload = () => {
+                this.SCRIPTS[idg].loaded = true;
+            };
+            this.document.head.appendChild(script);
+        }
+    }
 }
